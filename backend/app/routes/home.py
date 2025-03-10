feab7d66723f5cd3a039b39cb74e5b167c95451f
Jerin <jerinthomascarmel@gmail.com>
2025-03-10 16:16:04 +0530

    homepage done

--- backend/app/routes/home.py
+++ backend/app/routes/home.py
@@ -1,4 +1,14 @@
-from flask import jsonify
+from flask import render_template
+from flask_jwt_extended import get_jwt_identity, verify_jwt_in_request
+
 
 def home():
-    return jsonify({"message": "Welcome to the File Processor API!"})
+    # Try to get user identity, but don't require it
+    user = None
+    try:
+        verify_jwt_in_request(optional=True)
+        user = get_jwt_identity()
+    except:
+        pass
+
+    return render_template('home.html', user=user)
