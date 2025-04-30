f38925a43e675ec98e7ccb82c7d6f3bfd296fb3f
Jerin <jerinthomascarmel@gmail.com>
2025-04-30 11:31:55 +0530

    run()

--- backend/run.py
+++ backend/run.py
@@ -1,12 +1,5 @@
 from app import create_app
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 4000))
-    app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+    app.run()
